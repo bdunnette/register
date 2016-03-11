905993bf8c3814bc17d3116896257e0b3986e367
Brian Dunnette <dunn0172@umn.edu>
2016-03-11 16:47:21 -0600

    adding bootstrap, validation utility functions

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -1,10 +1,19 @@
 function(newDoc, oldDoc, userCtx, secObj) {
-  function require(field, message) {
+  function required(field, message) {
     message = message || "Document must have a " + field;
-    if (!newDoc[field]) throw({forbidden : message});
+    if (!newDoc[field]) throw ({
+      forbidden: message
+    });
+  };
+
+  function unchanged(field) {
+    if (oldDoc && toJSON(oldDoc[field]) != toJSON(newDoc[field]))
+      throw ({
+        forbidden: "Field can't be changed: " + field
+      });
   };
 
   if (newDoc.transaction_type || newDoc.type === "transaction") {
-    require("date");
+    required("date");
   }
 }
